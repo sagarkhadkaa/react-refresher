1d136961c739e545f4b0c232a532f07f271c9022
sagar <sagarkhadka2069@gmail.com>
2021-12-12 17:50:44 +0545

    React Refresher start

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const string = <h1>Say Hi to the React Refresher</h1>;
-ReactDOM.render(string, document.getElementById("root"));
+const element = <h1>Say Hi to the React Refresher</h1>;
+ReactDOM.render(element, document.getElementById("root"));
